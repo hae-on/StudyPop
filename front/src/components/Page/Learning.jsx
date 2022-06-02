ecbf35c0b31451cb72e3a1e95cdfcff312a6c1a6
hae-on <solbi2004@naver.com>
2022-06-02 22:56:33 +0900

    router 연결을 위한 임시페이지 생성

---
+++ front/src/components/Page/Learning.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Info = () => {
+  <p>Learning</p>;
+};
+
+export default Info;
---
+++ front/src/components/Page/Testing.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Info = () => {
+  <p>Testing</p>;
+};
+
+export default Info;
