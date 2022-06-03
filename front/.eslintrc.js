6f80ab248836f780276d66208594aea7757d0e50
hae-on <solbi2004@naver.com>
2022-06-03 18:29:39 +0900

    storybook 설치

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -2,21 +2,16 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'airbnb-base',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'airbnb-base', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
@@ -26,13 +21,13 @@ module.exports = {
     'import/extensions': 0,
     'no-use-before-define': 0,
     'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    'import/no-extraneous-dependencies': 0,
+    // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
     'no-shadow': 0,
     'react/prop-types': 0,
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
-  },
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }],
+    'jsx-a11y/no-noninteractive-element-interactions': 0
+  }
 };
