97c9b6cc7cd1dc38d2165473603f156deaffb95d
hae-on <solbi2004@naver.com>
2022-06-07 21:10:07 +0900

    sub subject에 따른 question list 분류

--- front/src/components/Molecule/table.tsx
+++ front/src/components/Molecule/table.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import dummy from '../../db/data.json';
 
-const Table = () => (
-  <table>
-    <tbody>
-      {dummy.questions.map((question) => (
-        <tr key={question.id}>
-          <td>{question.word}</td>
-          <td>{question.meaning}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+const Table = () => {
+  const subSubject = '데이터베이스 기본';
+  const questionList = dummy.questions.filter(
+    (question) => question.subSubject === subSubject,
+  );
+
+  return (
+    <table>
+      <tbody>
+        {questionList.map((question) => (
+          <tr key={question.id}>
+            <td>{question.word}</td>
+            <td>{question.meaning}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
