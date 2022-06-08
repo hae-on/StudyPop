41f09c88ec5010bb41c06d191d68deb5f3991f1a
hae-on <solbi2004@naver.com>
2022-06-08 14:49:53 +0900

    testing layout

--- front/src/components/Page/Testing.tsx
+++ front/src/components/Page/Testing.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import AddButtonBox from '../Molecule/addButtonBox';
+import styled from 'styled-components';
 import Menu from '../Organism/menu';
 
 const Testing = () => (
-  <>
-    <AddButtonBox />
-    <Menu />
-  </>
+  <Container>
+    <MenuPart>
+      <Menu />
+    </MenuPart>
+    <MainPart></MainPart>
+  </Container>
 );
 
 export default Testing;
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+`;
+
+const MenuPart = styled.div`
+  width: 30%;
+  margin-left: 50px;
+`;
+const MainPart = styled.div`
+  width: 70%;
+`;
