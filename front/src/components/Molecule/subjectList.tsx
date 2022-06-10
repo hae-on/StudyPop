85a2f283d7a9d9303c64b2aae2b9efe01df7952c
hae-on <solbi2004@naver.com>
2022-06-10 20:27:32 +0900

    subject data fetch and style

--- front/src/components/Molecule/subjectList.tsx
+++ front/src/components/Molecule/subjectList.tsx
@@ -1,29 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import dummy from '../../db/data.json';
 import Subject from '../Atom/subject';
 
-const SubjectList: React.FC = () => (
-  <SubjectDiv>
-    {dummy.subjects.length > 0 ? (
-      dummy.subjects.map((subject) => (
-        <Subject subject={subject} key={subject.id}></Subject>
-      ))
-    ) : (
-      <NoSubject>과목을 생성해주세요</NoSubject>
-    )}
-  </SubjectDiv>
-);
+const SubjectList: React.FC = () => {
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/subjects')
+      .then((res) => res.json())
+      .then((data) => {
+        setSubjects(data);
+      });
+  }, []);
+
+  return (
+    <SubjectDiv>
+      {subjects.length > 0 ? (
+        subjects.map((subject) => (
+          <SubjectContainer key={subject.id}>
+            <Subject subject={subject} />
+          </SubjectContainer>
+        ))
+      ) : (
+        <NoSubject>과목을 생성해주세요</NoSubject>
+      )}
+    </SubjectDiv>
+  );
+};
 
 export default SubjectList;
 
 const SubjectDiv = styled.ul`
   width: 100%;
-  padding-top: 8%;
+`;
+
+const SubjectContainer = styled.div`
+  padding-left: 15px;
+  &:first-child {
+    padding-top: 8%;
+  }
 `;
 
 const NoSubject = styled.div`
+  padding-top: 0;
   padding: 8%;
   color: #58eaac;
   font-size: 1.3rem;
