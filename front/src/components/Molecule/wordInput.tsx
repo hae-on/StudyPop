bb7aeb041b941cf037c58396d69e4ee04cc975c8
hae-on <solbi2004@naver.com>
2022-06-13 23:13:00 +0900

    문제 생성

--- front/src/components/Molecule/wordInput.tsx
+++ front/src/components/Molecule/wordInput.tsx
@@ -10,7 +10,25 @@ const WordInput: React.FC = () => {
 
     console.log(wordRef.current.value);
     console.log(meanRef.current.value);
-    console.log(subjectRef.current.value);
+    console.log(subjectRef.current.value.split(' - ')[1]);
+
+    fetch('http://localhost:3001/questions/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        subject: subjectRef.current.value.split(' - ')[0],
+        subSubject: subjectRef.current.value.split(' - ')[1],
+        word: wordRef.current.value,
+        meaning: meanRef.current.value,
+        isDone: false,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        alert('생성 완료');
+      }
+    });
   }
 
   const wordRef = useRef<HTMLInputElement>(null);
