cc9f63cf96ef5a52ecf851eacc75b8db9197f7a5
hae-on <solbi2004@naver.com>
2022-06-16 18:11:45 +0900

    select style 수정

--- front/src/components/Atom/selectInput.tsx
+++ front/src/components/Atom/selectInput.tsx
@@ -20,28 +20,36 @@ const SelectInput = React.forwardRef(
     const subjects = useFetch('http://localhost:3001/subjects');
 
     return (
-      <div>
+      <InputContainer>
         <InputName>{name} : </InputName>
-        <SelectBox ref={ref}>
-          {subjects.map(
-            (subject) =>
-              // eslint-disable-next-line implicit-arrow-linebreak
-              subject.subSubjects.map(
-                (subSubject: subSubjectType, index: number) => (
-                  <option
-                    key={index}
-                  >{`${subject.subject} - ${subSubject.title}`}</option>
-                ),
-              ),
-            // eslint-disable-next-line function-paren-newline
-          )}
-        </SelectBox>
-      </div>
+        <WholeInput>
+          <InputBox>
+            <SelectBox ref={ref}>
+              {subjects.map(
+                (subject) =>
+                  // eslint-disable-next-line implicit-arrow-linebreak
+                  subject.subSubjects.map(
+                    (subSubject: subSubjectType, index: number) => (
+                      <option
+                        key={index}
+                      >{`${subject.subject} - ${subSubject.title}`}</option>
+                    ),
+                  ),
+                // eslint-disable-next-line function-paren-newline
+              )}
+            </SelectBox>
+          </InputBox>
+        </WholeInput>
+      </InputContainer>
     );
   },
 );
 
 export default SelectInput;
+
+const InputContainer = styled.div`
+  display: flex;
+`;
 
 const InputName = styled.label`
   color: #58eaac;
@@ -50,13 +58,33 @@ const InputName = styled.label`
   margin-right: 1%;
 `;
 
-const SelectBox = styled.select`
-  width: 318px;
-  height: 36px;
-  font-size: 1.1rem;
-  padding-left: 10px;
+const WholeInput = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-  :focus {
-    outline: 1px solid #58eaac;
+const InputBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 300px;
+  padding: 10px;
+  border: 1px solid #4b4b4b;
+  border-radius: 3px;
+  z-index: 3;
+
+  &:focus-within {
+    border: 1px solid #58eaac;
+    outline: none;
   }
+`;
+
+const SelectBox = styled.select`
+  flex: 1 0 0;
+  margin: 0;
+  padding: 0;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  font-size: 1.1rem;
+  color: #4b4b4b;
 `;
