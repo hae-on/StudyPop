d6ffccd38ea3cc09e08fcd96e36e186a949caa3b
hae-on <solbi2004@naver.com>
2022-06-16 18:29:01 +0900

    subject post

--- front/src/components/Atom/dropdownInput.tsx
+++ front/src/components/Atom/dropdownInput.tsx
@@ -6,109 +6,113 @@ interface InputType {
   name: string;
 }
 
-const DropdownInput: React.FC<InputType> = ({ name }) => {
-  const subjects = useFetch('http://localhost:3001/subjects');
-  const subject = subjects.map((subject) => subject.subject);
+// eslint-disable-next-line react/display-name
+const DropdownInput = React.forwardRef(
+  ({ name }: InputType, ref?: React.Ref<HTMLInputElement>) => {
+    const subjects = useFetch('http://localhost:3001/subjects');
+    const subject = subjects.map((subject) => subject.subject);
 
-  const [inputValue, setInputValue] = useState('');
-  const [isHaveInputValue, setIsHaveInputValue] = useState(false);
-  const [dropDownList, setDropDownList] = useState(subject);
-  const [dropDownItemIndex, setDropDownItemIndex] = useState(-1);
+    const [inputValue, setInputValue] = useState('');
+    const [isHaveInputValue, setIsHaveInputValue] = useState(false);
+    const [dropDownList, setDropDownList] = useState(subject);
+    const [dropDownItemIndex, setDropDownItemIndex] = useState(-1);
 
-  const showDropDownList = () => {
-    if (inputValue === '') {
+    const showDropDownList = () => {
+      if (inputValue === '') {
+        setIsHaveInputValue(false);
+        setDropDownList([]);
+      } else {
+        const choosenTextList = subject.filter(
+          (textItem) =>
+            // eslint-disable-next-line implicit-arrow-linebreak
+            textItem.includes(inputValue),
+          // eslint-disable-next-line function-paren-newline
+        );
+        setDropDownList(choosenTextList);
+      }
+    };
+
+    const changeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(event.currentTarget.value);
+      setIsHaveInputValue(true);
+    };
+
+    const clickDropDownItem = (clickedItem: React.SetStateAction<string>) => {
+      setInputValue(clickedItem);
       setIsHaveInputValue(false);
-      setDropDownList([]);
-    } else {
-      const choosenTextList = subject.filter(
-        (textItem) =>
-          // eslint-disable-next-line implicit-arrow-linebreak
-          textItem.includes(inputValue),
-        // eslint-disable-next-line function-paren-newline
-      );
-      setDropDownList(choosenTextList);
-    }
-  };
+    };
 
-  const changeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.currentTarget.value);
-    setIsHaveInputValue(true);
-  };
+    const handleDropDownKey = (event: KeyboardEvent) => {
+      if (isHaveInputValue) {
+        if (
+          // eslint-disable-next-line operator-linebreak
+          event.key === 'ArrowDown' &&
+          dropDownList.length - 1 > dropDownItemIndex
+        ) {
+          setDropDownItemIndex(dropDownItemIndex + 1);
+        }
 
-  const clickDropDownItem = (clickedItem: React.SetStateAction<string>) => {
-    setInputValue(clickedItem);
-    setIsHaveInputValue(false);
-  };
-
-  const handleDropDownKey = (event: KeyboardEvent) => {
-    if (isHaveInputValue) {
-      if (
-        // eslint-disable-next-line operator-linebreak
-        event.key === 'ArrowDown' &&
-        dropDownList.length - 1 > dropDownItemIndex
-      ) {
-        setDropDownItemIndex(dropDownItemIndex + 1);
+        if (event.key === 'ArrowUp' && dropDownItemIndex >= 0) {
+          setDropDownItemIndex(dropDownItemIndex - 1);
+        }
+        if (event.key === 'Enter' && dropDownItemIndex >= 0) {
+          clickDropDownItem(dropDownList[dropDownItemIndex]);
+          setDropDownItemIndex(-1);
+        }
       }
+    };
 
-      if (event.key === 'ArrowUp' && dropDownItemIndex >= 0) {
-        setDropDownItemIndex(dropDownItemIndex - 1);
-      }
-      if (event.key === 'Enter' && dropDownItemIndex >= 0) {
-        clickDropDownItem(dropDownList[dropDownItemIndex]);
-        setDropDownItemIndex(-1);
-      }
-    }
-  };
+    useEffect(showDropDownList, [inputValue]);
 
-  useEffect(showDropDownList, [inputValue]);
-
-  return (
-    <>
-      <MajorSubject>
-        <InputName>{name}</InputName>
-        <WholeSubjectInput>
-          <InputBox>
-            <Input
-              type="text"
-              value={inputValue}
-              onChange={changeInputValue}
-              onKeyUp={handleDropDownKey}
-            ></Input>
-            <DeleteButton onClick={() => setInputValue('')}>
-              &times;
-            </DeleteButton>
-          </InputBox>
-          {isHaveInputValue && (
-            <DropDownBox>
-              {dropDownList.length === 0 && (
-                <DropDownItem>해당하는 단어가 없습니다</DropDownItem>
-              )}
-              {dropDownList.map((dropDownItem, dropDownIndex) => (
-                <DropDownItem
-                  key={dropDownIndex}
-                  onClick={() => clickDropDownItem(dropDownItem)}
-                  onMouseOver={() => setDropDownItemIndex(dropDownIndex)}
-                  className={
-                    dropDownItemIndex === dropDownIndex ? 'selected' : ''
-                  }
-                >
-                  {dropDownItem}
-                </DropDownItem>
-              ))}
-            </DropDownBox>
-          )}
-        </WholeSubjectInput>
-      </MajorSubject>
-    </>
-  );
-};
+    return (
+      <>
+        <MajorSubject>
+          <InputName>{name}</InputName>
+          <WholeSubjectInput>
+            <InputBox>
+              <Input
+                type="text"
+                value={inputValue}
+                ref={ref}
+                onChange={changeInputValue}
+                onKeyUp={handleDropDownKey}
+              ></Input>
+              <DeleteButton onClick={() => setInputValue('')}>
+                &times;
+              </DeleteButton>
+            </InputBox>
+            {isHaveInputValue && (
+              <DropDownBox>
+                {dropDownList.length === 0 && (
+                  <DropDownItem>해당하는 단어가 없습니다</DropDownItem>
+                )}
+                {dropDownList.map((dropDownItem, dropDownIndex) => (
+                  <DropDownItem
+                    key={dropDownIndex}
+                    onClick={() => clickDropDownItem(dropDownItem)}
+                    onMouseOver={() => setDropDownItemIndex(dropDownIndex)}
+                    className={
+                      dropDownItemIndex === dropDownIndex ? 'selected' : ''
+                    }
+                  >
+                    {dropDownItem}
+                  </DropDownItem>
+                ))}
+              </DropDownBox>
+            )}
+          </WholeSubjectInput>
+        </MajorSubject>
+      </>
+    );
+  },
+);
 
 export default DropdownInput;
 
 const MajorSubject = styled.div`
   display: flex;
   margin-bottom: 3%;
-  margin-left: -2%;
+  margin-left: -19px;
 `;
 
 const WholeSubjectInput = styled.div`
--- front/src/components/Molecule/subjectInput.tsx
+++ front/src/components/Molecule/subjectInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import DropdownInput from '../Atom/dropdownInput';
 import Input from '../Atom/input';
@@ -7,13 +7,34 @@ import CreateButtonBox from './createButtonBox';
 const SubjectInput: React.FC = () => {
   function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    console.log(subjectRef.current.value);
+    console.log(subSubjectRef.current.value);
+
+    fetch('http://localhost:3001/subjects/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        subject: subjectRef.current.value,
+        subSubject: subSubjectRef.current.value,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        alert('생성 완료');
+      }
+    });
   }
+
+  const subjectRef = useRef<HTMLInputElement>(null);
+  const subSubjectRef = useRef<HTMLInputElement>(null);
 
   return (
     <form onSubmit={onSubmit}>
       <InputPart>
-        <DropdownInput name="대과목 :" />
-        <Input name="과목 " />
+        <DropdownInput name="대과목 :" ref={subjectRef} />
+        <Input name="과목 " ref={subSubjectRef} />
       </InputPart>
       <CreateButtonBox />
     </form>
