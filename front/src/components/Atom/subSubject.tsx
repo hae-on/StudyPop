54c989e09a50080a867f186ed9c6172e53cdbfc8
hae-on <solbi2004@naver.com>
2022-06-20 22:26:42 +0900

    creat mode에는 menu 선택 금지

--- front/src/components/Atom/subSubject.tsx
+++ front/src/components/Atom/subSubject.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable indent */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 interface subSubjectType {
@@ -23,9 +26,14 @@ const SubSubject: React.FC<Props> = (props) => {
       {subSubjects ? (
         subSubjects.map((subSubject, index) => (
           <SubsubjectItem key={index} dropdown={dropdown}>
-            <StyledLink to={`${subSubject.title}`}>
-              {subSubject.title}
-            </StyledLink>
+            {useLocation().pathname === '/create_word' ||
+            useLocation().pathname === '/create_subject' ? (
+              <div>{subSubject.title}</div>
+            ) : (
+              <StyledLink to={`${subSubject.title}`}>
+                {subSubject.title}
+              </StyledLink>
+            )}
           </SubsubjectItem>
         ))
       ) : (
